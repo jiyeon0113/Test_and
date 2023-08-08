6619733cdf6edd1bfc44a64ad79691cf20a6c1bd
jiyeon0113 <jeny51264520@gmail.com>
2023-08-08 18:49:52 +0900

    update for main

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,54 +1,76 @@
-import {  View, Text, StyleSheet, Image } from "react-native";
+import {View, Text, StyleSheet, Image} from 'react-native';
 import * as React from 'react';
-import Input, { KeyboardTypes, ReturnKeyTypes } from '../components/Input';
-import { Button } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import Input, {KeyboardTypes, ReturnKeyTypes} from '../components/Input';
+import {Button} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {useState} from 'react';
+import Home from './Home';
 
-
-const Login = ( ) =>  {
+const Login = () => {
     const navigation = useNavigation();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const isEmailValid = email => {
+        const emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+        return emailPattern.test(email);
+    };
+
+    const isLoginEnabled = () => {
+        return email.length > 0 && password.length > 0 && isEmailValid(email);
+    };
 
     const hanndleTermsScreen = () => {
         navigation.navigate('TermsScreen');
     };
 
-    return(
-        <View style={styles.container}>            
-            <Image
-            source={require('../../assets/main.png')}
-            style={styles.image}
+    const handleHomeScreen = () => {
+        navigation.navigate('Home');
+    };
+
+    return (
+        <View style={styles.container}>
+        <Image source={require('../../assets/main.png')} style={styles.image} />
+
+        <Text style={styles.baseText}>GreenDan</Text>
+
+        <Input
+            title={'이메일'}
+            placeholder="your@mail.com"
+            keyboardType={KeyboardTypes.EMAIL}
+            returnKeyType={ReturnKeyTypes.NEXT}
+            value={email}
+            onChangeText={text => setEmail(text)}
+        />
+        <Input
+            title={'비밀번호'}
+            returnKeyType={ReturnKeyTypes.DONE}
+            secureTextEntry
+            value={password}
+            onChangeText={text => setPassword(text)}
+        />
+
+        <View style={{flexDirection: 'row'}}>
+            <Button
+            title="login"
+            modeValue="text"
+            onPress={handleHomeScreen}
+            style={styles.Button}
+            disabled={!isLoginEnabled()}
             />
 
-            <Text style={styles.baseText}>
-                GreenDan
-            </Text>
+            <Button
+            title="Sign Up"
+            omodeValue="text"
+            onPress={hanndleTermsScreen}
+            style={styles.Button}
+            />
+        </View>
+        </View>
+    );
+    };
 
-            <Input 
-                title={'이메일'} 
-                placeholder="your@mail.com" 
-                keyboardType={KeyboardTypes.EMAIL}
-                returnKeyType={ReturnKeyTypes.NEXT}
-                />
-            <Input title={'비밀번호'} 
-                returnKeyType={ReturnKeyTypes.DONE}
-                secureTextEntry
-                />
-
-                <Button
-                title="login"
-                modeValue='text'
-                />
-
-                <Button
-                title="Sign Up"
-                omodeValue='text'
-                onPress={hanndleTermsScreen}
-                />
-            </View>
-    );    
-}
-
-const styles = StyleSheet.create({
+    const styles = StyleSheet.create({
     baseText: {
         fontSize: 40,
     },
@@ -56,12 +78,12 @@ const styles = StyleSheet.create({
         backgroundColor: '#8CB972',
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
     },
     Button: {
-        backgroundColor: 'E5EFDF',
-        margin: 24,
-    }
+        backgroundColor: '#E5EFDF',
+        margin: 10,
+    },
 });
 
 export default Login;
